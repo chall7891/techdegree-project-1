958443d5fac2b415e99d068d622b14e6e02247d8
Collin Halliday <collinhalliday@gmail.com>
2017-11-30 19:27:27 -0700

    Added Project Files For Grading
    Project complete. Shooting for a grade of "exceeds expectations."

---
+++ js/quotation_generator.js
@@ -0,0 +1,71 @@
+var quotation;
+var colorInterval;
+var quoteInterval;
+
+//Generates random number between two specified parameters.
+function getRandomNumber( lower, upper ) {
+  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
+}
+
+//Selects a random quote object from the quotes array and returns it.
+function getRandomQuote() {
+  return quotes[getRandomNumber(0, quotes.length - 1)];
+}
+
+//Stores a random quote object in a variable.
+//Constructs a string containing the quote object's properties and displays it.
+function printQuote() {
+  quotation = getRandomQuote();
+  var html = '<p class="quote">' + quotation.quote + '</p>';
+  html += '<p class="source">' + quotation.source +
+    constructOptionalString('citation') +
+    constructOptionalString('year') +
+  '</p>';
+  html += '<p class="tag">' + quotation.tag + '</p>';
+  document.getElementById('quote-box').innerHTML = html;
+}
+
+//Constructs strings for the citation and year properties if present in a
+//particular quote object. If not present, returns an empty string.
+function constructOptionalString(property) {
+  if(quotation[property] !== undefined)
+    return '<span class="' + property + '">' + quotation[property] + '</span>';
+  else
+    return '';
+}
+
+//Generates random color using getRandomNumber() and decimal values for red,
+//green, and blue, and returns the random color.
+function getRandomColor() {
+  return 'rgb(' + getRandomNumber(0, 255) + ',' +
+                  getRandomNumber(0, 255) + ',' +
+                  getRandomNumber(0, 255) + ')';
+}
+
+//Changes background color for the body and button when called by changing the
+//innerHTML of the <style/> tags to the value assigned to the css variable.
+function changeBackgroundColor () {
+  var css = '.changeableBackground { background-color: ' + getRandomColor() + '}';
+  document.getElementById('backgroundColor').innerHTML = css;
+}
+
+//Calls printQuote() and changeBackgroundColor() every 10 seconds.
+function autoChange() {
+    colorInterval =  setInterval(changeBackgroundColor, 10000);
+    quoteInterval =  setInterval(printQuote, 10000);
+}
+
+//Resets the intervals in autoChange() when called (button push). This keeps a
+//a reset from occuring directly after a button push.
+function resetInterval() {
+  clearInterval(colorInterval);
+  clearInterval(quoteInterval);
+  autoChange();
+}
+
+//Creates event listeners to respond to "Show another quote" button clicks.
+//When button is clicked, printQuote(), changeBackgroundColor() and
+//resetInterval() are called.
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document.getElementById('loadQuote').addEventListener("click", changeBackgroundColor, false);
+document.getElementById('loadQuote').addEventListener("click", resetInterval, false);
---
+++ js/quotations.js
@@ -0,0 +1,66 @@
+//An array of objects that each include a quote, the quote's source,
+//and where available, its citation and year.
+var quotes = [
+  {
+    quote: 'Always be yourself, express yourself, have faith in yourself, ' +
+           'do not go out and look for a successful personality and duplicate it.',
+    source: 'Bruce Lee',
+
+    citation: '"Striking Thoughts: Bruce Lee\'s Wisdom for Daily Living"',
+
+    tag: '#BelieveInYourself'
+  },
+  {
+    quote: 'Study and in general the pursuit of truth and beauty is a sphere of activity ' +
+           'in which we are permitted to remain children all our lives.',
+    source: 'Albert Einstein',
+
+    citation: 'Letter to Adrianna Enriques, p. 83',
+
+    year: 'October, 1921',
+
+    tag: '#ForeverYoung'
+  },
+  {
+    quote: '(Piglet) How do you spell love? (Pooh) You don’t spell it, you feel it.',
+
+    source: 'A.A. Milne',
+
+    citation: 'Winnie the Pooh',
+
+    tag: '#Love'
+  },
+  {
+    quote: 'I have learned that success is to be measured not so much by the position ' +
+           'that one has reached in life as by the obstacles which he has overcome while trying to succeed.',
+    source: 'Booker T. Washington',
+
+    citation: 'Up from Slavery: an Autobiography',
+
+    year: 1901,
+
+    tag: '#Overcome'
+  },
+  {
+    quote: 'Genius is one percent inspiration, ninety-nine percent perspiration.',
+
+    source: 'Thomas Edison',
+
+    citation: 'Harper\'s Monthly',
+
+    year: 1903,
+
+    tag: '#NoLimits'
+  },
+  {
+    quote: 'You can do anything but not everything.',
+
+    source: 'David Allen',
+
+    citation: 'Making It All Work',
+
+    year: 2009,
+
+    tag: '#EndlessPossibilities'
+  }
+];
